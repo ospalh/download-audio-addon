e6a094b9132a5d51c5821156577dae8f9456f6f9
ospalh <ospalh@gmail.com>
2012-05-04 14:10:35 +0200

    Do-nothing files.

---
+++ downloadaudio/download.py
@@ -0,0 +1,11 @@
+# -*- mode: python ; coding: utf-8 -*-
+#!/usr/bin/env python
+# 
+# Copyright © 2012 Roland Sieker, <ospalh@gmail.com>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+
+
+# from process import ProcessAudio
+# from sitedownloader import Downloader
+
